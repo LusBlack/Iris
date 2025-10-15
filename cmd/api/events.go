f78a41e862b826b06d17633c447a2fbfd1c2ade0
Louis <Lou.ikwuegbu@gmail.com>
2025-10-15 23:31:55 +0100

    refactor event handlers for improved error handling and response structure. added update and get methods

--- cmd/api/events.go
+++ cmd/api/events.go
@@ -26,11 +26,22 @@ func (app *application) createEvent(c *gin.Context) {
 	c.JSON(http.StatusCreated, event)
 }
 
+func (app *application) getAllEvents(c *gin.Context) {
+	events, err := app.models.Events.GetAll()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve events"})
+	}
+
+	c.JSON(http.StatusOK, events)
+}
+
 func (app *application) getEvent(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
 
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+		return
 	}
 
 	event, err := app.models.Events.Get(id)
@@ -41,7 +52,43 @@ func (app *application) getEvent(c *gin.Context) {
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve event"})
+		return
 	}
 
 	c.JSON(http.StatusOK, event)
+}
+
+func (app *application) updateEvent(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+	}
+
+	existingEvent, err := app.models.Events.Get(id)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve event"})
+		return
+	}
+
+	if existingEvent == nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Event not found"})
+		return
+	}
+
+	updatedEvent := &database.Event{}
+
+	if err := c.ShouldBindJSON(updatedEvent); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	updatedEvent.Id = id
+
+	if err := app.models.Events.Update(updatedEvent); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update event"})
+		return
+	}
+
+	c.JSON(http.StatusOK, updatedEvent)
 }
